18c9322b9de2d34d34a8029efa5b65b432c6172a
nereazj544 <nzjvirtual@gmail.com>
2023-10-20 00:50:44 +0200

    ReclyclerView imagenes de los equipos puestas

--- app/src/main/java/com/example/contador_para_entregar/PracticasGuiadas/MainActivity.java
+++ app/src/main/java/com/example/contador_para_entregar/PracticasGuiadas/MainActivity.java
@@ -22,23 +22,24 @@ public class MainActivity extends AppCompatActivity {
         rv.setLayoutManager(new LinearLayoutManager(this));
         List<Equipos> l = Arrays.asList(
                 new Equipos("Alpino", R.drawable.alpino),
-                new Equipos("Royal Academy", R.drawable.alpino),
-                new Equipos("Raimon", R.drawable.alpino),
-                new Equipos("Luz Eterna", R.drawable.alpino),
-                new Equipos("Inazuma Battel Eleven", R.drawable.alpino),
-                new Equipos("Inazuma Best Eleven", R.drawable.alpino),
-                new Equipos("Equipo Zero", R.drawable.alpino),
-                new Equipos("Equipo Ogre", R.drawable.alpino),
-                new Equipos("Instituto Zeus", R.drawable.alpino),
-                new Equipos("Instituto Universal", R.drawable.alpino),
-                new Equipos("Resistencia de Japon", R.drawable.alpino),
-                new Equipos("Dragones de Fuego", R.drawable.alpino),
-                new Equipos("Namuhara", R.drawable.alpino),
-                new Equipos("Earth Eleven", R.drawable.alpino),
-                new Equipos("Inazuma Japon", R.drawable.alpino),
-                new Equipos("The Little Gigants", R.drawable.alpino),
-                new Equipos("Instituto Occult", R.drawable.alpino),
-                new Equipos("Unicorn", R.drawable.alpino)
+                new Equipos("Royal Academy", R.drawable.royalacademy),
+                new Equipos("Raimon", R.drawable.raimon),
+                new Equipos("Luz Eterna", R.drawable.Escudo_Luz_Eterna),
+                new Equipos("Inazuma Battel Eleven", R.drawable.Inazuma_Battle_Eleven_Emblema),
+                new Equipos("Inazuma Best Eleven", R.drawable.Inazuma_Best_Eleven_Emblema),
+                new Equipos("Equipo Zero", R.drawable.Equipo_Zero_Emblema),
+                new Equipos("Equipo Ogre", R.drawable.Ogro),
+                new Equipos("Instituto Zeus", R.drawable.Escudo_Zeus_FF),
+                new Equipos("Instituto Universal", R.drawable.Universal_Emblema),
+                new Equipos("Resistencia de Japon", R.drawable.Resistencia_Jap_Logo),
+                new Equipos("Dragones de Fuego", R.drawable.Dragones_de_Fuego_Emblema),
+                new Equipos("Namuhara", R.drawable.Nagumohara),
+                new Equipos("Earth Eleven", R.drawable.Earth_Eleven_Emblema),
+                new Equipos("Inazuma Japon", R.drawable.inazuma_japon),
+                new Equipos("The Little Gigants", R.drawable.little_gigants),
+                new Equipos("Instituto Occult", R.drawable.Occult_Emblema),
+                new Equipos("Unicorn", R.drawable.Unicorn_Emblema),
+                new Equipos("Aullido Lunar", R.drawable.Aullido_Lunar_Emblema)
         );
         rv.setAdapter(new EquipoAdarer(l));
 
