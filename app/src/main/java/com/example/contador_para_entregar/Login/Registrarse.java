f90bc5d4c16f7b5dd2c01a93f5850d7ef714e2b8
nereazj544 <nzjvirtual@gmail.com>
2023-11-23 16:33:43 +0100

    ACTUAL: SQLite NOTA: Intneto de modificaciones en la tienda, pero estoy hasta el cu... de esto :)

--- app/src/main/java/com/example/contador_para_entregar/Login/Registrarse.java
+++ app/src/main/java/com/example/contador_para_entregar/Login/Registrarse.java
@@ -2,8 +2,11 @@ package com.example.contador_para_entregar.Login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.contador_para_entregar.PantallaActivity;
 import com.example.contador_para_entregar.R;
 
 public class Registrarse extends AppCompatActivity {
@@ -12,5 +15,22 @@ public class Registrarse extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrarse);
+    }
+
+
+
+
+
+    public  void  cuenta(View v){
+        //Esto va a ir a la otra pantalla
+        Intent in = new Intent(this, InicarSesion.class);
+        startActivity(in);
+        finish();
+    }
+
+    public void iniciar(View v){
+        Intent log = new Intent(this, PantallaActivity.class);
+        startActivity(log);
+        finish();
     }
 }
