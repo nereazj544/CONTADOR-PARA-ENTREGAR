ea702efa66153fb1c79dcd005bf30c04f60125bd
nereazj544 <nzjvirtual@gmail.com>
2023-11-29 12:39:39 +0100

    ACTUAL: SQLite NOTA: Intneto de modificaciones en la tienda, pero estoy hasta el cu... de esto :)

--- app/src/main/java/com/example/contador_para_entregar/Login/InicarSesion.java
+++ app/src/main/java/com/example/contador_para_entregar/Login/InicarSesion.java
@@ -1,10 +1,11 @@
 package com.example.contador_para_entregar.Login;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.contador_para_entregar.PantallaActivity;
 import com.example.contador_para_entregar.R;
@@ -19,16 +20,5 @@ public class InicarSesion extends AppCompatActivity {
 
 
 
-    public  void  nocuenta(View v){
-        //Esto va a ir a la otra pantalla
-        Intent in = new Intent(this, Registrarse.class);
-        startActivity(in);
-        finish();
-    }
 
-    public void loging(View v){
-        Intent log = new Intent(this, PantallaActivity.class);
-        startActivity(log);
-        finish();
-    }
 }
