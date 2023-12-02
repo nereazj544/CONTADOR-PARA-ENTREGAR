988b6a26e91c91da05b49a6991101bc16ddc8ec9
nereazj544 <nzjvirtual@gmail.com>
2023-12-02 17:01:21 +0100

    ACTUAL: SQLite NOTA: Tienda sigue sin tener modificaciones. :)

---
+++ app/src/main/java/com/example/contador_para_entregar/Login/Database_Helper.java
@@ -0,0 +1,29 @@
+package com.example.contador_para_entregar.Login;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class Database_Helper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1; //vesrion de la Database
+    public static final String DATABASE_NAME = "usuarios.db"; //nombre de la base
+    public Database_Helper(@Nullable Context context, @Nullable String name, 
+                           @Nullable SQLiteDatabase.CursorFactory factory, 
+                           int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(Database_estrucute.SQL_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    db.execSQL(Database_estrucute.SQL_DELETE);
+    onCreate(db);
+    }
+  
+}
