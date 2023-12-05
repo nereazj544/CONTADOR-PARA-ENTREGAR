b284304445ec3c3b69a0648352c6f8aa5373b61d
nereazj544 <nzjvirtual@gmail.com>
2023-12-05 14:54:28 +0100

    ACTUAL: SQLite NOTA: Tienda sigue sin tener modificaciones. :)

--- app/src/main/java/com/example/contador_para_entregar/Login/Registrarse.java
+++ app/src/main/java/com/example/contador_para_entregar/Login/Registrarse.java
@@ -2,33 +2,67 @@ package com.example.contador_para_entregar.Login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.contador_para_entregar.PantallaActivity;
 import com.example.contador_para_entregar.R;
 
 public class Registrarse extends AppCompatActivity {
 
+    Button resesion;
+    EditText reuser, recontra;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrarse);
+
+        recontra = (EditText) findViewById(R.id.contra);
+        reuser = (EditText) findViewById(R.id.name_user);
+        resesion = (Button) findViewById(R.id.iniciar);
+
+        final Database_Helper dbHelper = new Database_Helper(this);
+
+        resesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues  values = new ContentValues();
+                values.put(Database_estrucute.FeedEntry.CAMPO2,reuser.getText().toString());
+                values.put(Database_estrucute.FeedEntry.CAMPO3,recontra.getText().toString());
+
+                long newRowld = db.insert(Database_estrucute.FeedEntry.TB_NAME,
+                        null, values);
+
+                Toast.makeText(getApplicationContext(),
+                        "Usuario registrado" + newRowld, Toast.LENGTH_SHORT).show();
+
+                reuser.setBackgroundColor(getResources().getColor(R.color.white));
+            }
+        });
+
     }
 
+
+
+
+
     public  void  cuenta(View v){
-        //Esto va a ir a la otra pantalla
+        //Para ir a la iniciar sesion de la cuenta.
         Intent in = new Intent(this, MainActivityLogin.class);
         startActivity(in);
         finish();
     }
 
-    public void iniciar(View v){
-        Intent log = new Intent(this, PantallaActivity.class);
-        startActivity(log);
-        finish();
-    }
+
 
 
 
